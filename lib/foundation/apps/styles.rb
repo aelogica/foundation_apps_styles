78135bfb6f4f0b7bd81e0d490657fe3f692dfe24
Eumir Gaspar <eumir@aelogica.com>
2015-06-15 11:26:11 +0800

    Initial commit

---
+++ lib/foundation/apps/styles.rb
@@ -0,0 +1,9 @@
+require "foundation/apps/styles/version"
+
+module Foundation
+  module Apps
+    module Styles
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/foundation/apps/styles/version.rb
@@ -0,0 +1,7 @@
+module Foundation
+  module Apps
+    module Styles
+      VERSION = "0.1.0"
+    end
+  end
+end
